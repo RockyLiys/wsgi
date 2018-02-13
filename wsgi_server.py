2d44677d0933444ead5efcbff8b4da30b16ab29a
RockyLiys <liys_liys@163.com>
2018-02-13 17:41:06 +0800

    modify server

--- wsgi_server.py
+++ wsgi_server.py
@@ -3,6 +3,7 @@ from wsgiref import simple_server as ss
 
 def server():
     server = ss.make_server("0.0.0.0", 9898, ss.demo_app)
+    print "Webbrower url port :9898 ......."
     server.serve_forever()
 
 server()
